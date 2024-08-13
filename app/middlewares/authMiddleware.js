638588f2a90ffff5e05ce7205f6e4bddd9c0e541
nikolg0 <nikig0731@gmail.com>
2024-08-13 20:39:33 +0200

    commit

--- app/middlewares/authMiddleware.js
+++ app/middlewares/authMiddleware.js
@@ -3,14 +3,11 @@ const User = require("../models/Users");
 
 module.exports = (req, res, next) => {
   const token = req.cookies["AuthToken"];
-  console.log("token otrzymany");
   if (token) {
     try {
       const verified = jwt.verify(token, "secretKey");
-      console.log("token zweryfikowany");
       User.findById(verified._id)
         .then((user) => {
-          console.log("użytkownik znaleziony");
           res.locals.userId = user._id;
           res.locals.userName = user.name;
           next();
@@ -19,7 +16,6 @@ module.exports = (req, res, next) => {
           res.send(err);
         });
     } catch {
-      console.log("błąd");
       res.redirect("/auth/login?loginRedirect=true");
     }
   } else {
--- app/router/userRouter.js
+++ app/router/userRouter.js
@@ -3,17 +3,32 @@ const router = express.Router();
 
 const userController = require("../controllers/userController");
 
+router.use("/", (req, res, next) => {
+  if (req.path === "/" || req.path === "") {
+    return res.redirect("auth/login");
+  }
+  next();
+});
+
+function redirectIfLogged(req, res, next) {
+  if (req.cookies && req.cookies.AuthToken) {
+    return res.redirect("/");
+  }
+  next();
+}
+
 router.get("/signup", (_req, res) => {
   res.render("userViews/signupUser", { style: "signupUser.css" });
 });
 
 router.post("/signup", userController.create);
 
-router.get("/login", (req, res) => {
+router.get("/login", redirectIfLogged, (req, res) => {
   if (req.query.loginRedirect) {
     res.render("userViews/loginUser", {
       error: true,
       message: "Proszę się zalogować",
+      style: "loginUser.css",
     });
     return;
   }
